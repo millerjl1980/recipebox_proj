5b13e06539f36a0f6f08a7bce50ebf3e4d386bcb
Justin Miller <indymiller@gmail.com>
2020-05-04 22:32:15 -0400

    assignment #2 completed

--- recipe/urls.py
+++ recipe/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('recipe/<int:id>', views.recipe, name='recipe'),
     path('author/<int:id>', views.author, name='author'),
+    path('addauthor.html', views.add_author, name='addauthor'),
+    path('addrecipe.html', views.add_recipe, name='addrecipe'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from recipe.forms import AddRecipeForm, AddAuthorForm
 from recipe.models import Author, Recipe
 
 # Create your views here.
@@ -19,3 +19,23 @@ def recipe(request, id):
 def author(request, id):
     author = get_object_or_404(Author, pk=id)
     return render(request, 'author.html', {'author': author})
+
+def add_author(request):
+    if request.method == 'POST':
+        form = AddAuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = AddAuthorForm()
+    return render(request, 'addauthor.html', {'form': form})
+
+def add_recipe(request):
+    if request.method == 'POST':
+        form = AddRecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = AddRecipeForm()
+    return render(request, 'addrecipe.html', {'form': form})
