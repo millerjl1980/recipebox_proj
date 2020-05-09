9d253ab10e4f26782535590912c042234867048a
Justin Miller <indymiller@gmail.com>
2020-05-09 15:19:46 -0400

    assignment completed

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,14 +1,25 @@
 from django import forms
 from django.forms import modelform_factory
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 from recipe.models import Author, Recipe
 
 # Since there were no fields in the models that were goiing to be excluded
 # used modelform_factory to make forms with
 
-AddAuthorForm = modelform_factory(Author, exclude=[])
 
 AddRecipeForm = modelform_factory(Recipe, exclude=[])
+
+# AddAuthorForm = modelform_factory(Author, exclude=['user'])
+
+class AddAuthorForm(UserCreationForm):
+    name = forms.CharField(max_length=50)
+    bio = forms.CharField()
+    class Meta:
+        model = User
+        fields = ('name', 'bio', 'username', 'password1', 'password2')
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=50)
--- recipe/views.py
+++ recipe/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse, HttpResponseRedirect
 from recipe.forms import AddRecipeForm, AddAuthorForm, LoginForm
 from recipe.models import Author, Recipe
+from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -15,9 +19,19 @@ def loginview(request):
                 )
             if user:
                 login(request, user)
-                return HttpResponseRedirect(reverse('home'))
+                return HttpResponseRedirect(
+                    request.GET.get('next', reverse('home'))
+                )
     form = LoginForm()
     return render(request, 'generic_form.html', {'form': form})
+
+# https://pythonprogramming.net/user-login-logout-django-tutorial/
+def logoutview(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return HttpResponseRedirect(
+                    request.GET.get('next', reverse('home'))
+                )
 
 def index(request):
     return render(request, 'index.html',
@@ -35,16 +49,27 @@ def author(request, id):
     recipes = Recipe.objects.filter(author=author)
     return render(request, 'author.html', {'author': author, 'recipes': recipes})
 
+# https://www.youtube.com/watch?time_continue=399&v=Tja4I_rgspI&feature=emb_logo
+# https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
+@login_required
 def add_author(request):
     if request.method == 'POST':
         form = AddAuthorForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            user.refresh_from_db()
+            user.author.name = form.cleaned_data.get('name')
+            user.author.bio = form.cleaned_data.get('bio')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
             return redirect('home')
     else:
         form = AddAuthorForm()
     return render(request, 'addauthor.html', {'form': form})
 
+@login_required
 def add_recipe(request):
     if request.method == 'POST':
         form = AddRecipeForm(request.POST)
